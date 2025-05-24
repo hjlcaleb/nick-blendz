396a60876143f4a228aabd0d469e409ba985bd3e
Caleb Hu <chu552@cs.washington.edu>
2025-05-24 01:47:51 -0700

    fixed centering issue and added ig link

--- frontend/web-pages/src/app/page.tsx
+++ frontend/web-pages/src/app/page.tsx
@@ -18,12 +18,18 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <NavBar />
-        <h1 className="font-bold">FOLLOW ME ON INSTAGRAM</h1>
-        <h2 className="">@nickkblendz</h2>
-        <div className="sticky grid grid-cols-2 sm:grid-cols-4 gap-4 position-fixed">
-          {instagramImageUrls.map((url, index) => (
-            <InstagramPhoto key={index} src={url} alt={"Instagram post"} width={300} height={300} />
-          ))}
+        <div className="text-center">
+          <h1 className="font-bold">FOLLOW ME ON INSTAGRAM</h1>
+          <a target="_blank" href="https://www.instagram.com/nickkblendz/">
+            <h3 className="underline">@nickkblendz</h3>
+          </a>
+
+          <div className="sticky grid grid-cols-2 sm:grid-cols-4 gap-4 position-fixed">
+            {instagramImageUrls.map((url, index) => (
+              <InstagramPhoto key={index} src={url} alt={"Instagram post"} width={300} height={300} />
+            ))}
+          </div>
+
         </div>
 
         <div id="section4">
