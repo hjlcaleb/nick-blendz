42667aeb908cf633dc1b7af41d01a6636e484928
hjlcaleb <calebhu552@gmail.com>
2025-05-26 17:11:16 -0700

    Revert "Caleb"

--- frontend/web-pages/src/app/services/page.tsx
+++ frontend/web-pages/src/app/services/page.tsx
@@ -1,13 +1,14 @@
+'use client';
 
-import NavBar from './components/navbar';
-import Footer from './components/footer';
+import NavBar from '../components/navbar';
+import Footer from '../components/footer';
 
 export default function Services() {
   return (
     <div className="flex flex-col min-h-screen">
       <NavBar />
 
-      <main className="flex-grow flex flex-col items-center p-8 pb-20 gap-8 sm:p-20 pt-20">
+      <main className="flex-grow flex flex-col items-center p-8 pb-20 gap-8 sm:p-20">
         {/* Page Title */}
         <div className="w-full text-center mt-8">
           <h1 className="font-bold text-2xl sm:text-3xl md:text-4xl tracking-wide">SERVICES</h1>
